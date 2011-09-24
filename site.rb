ab26a0fb8c3bd3dffbada0565a4b8a082127271c
Nat Welch <nat@natwelch.com>
2011-09-24 15:17:12 -0700

    Use bundler properly.

--- site.rb
+++ site.rb
@@ -2,23 +2,6 @@
 # An app for ...
 # @author Nat Welch - https://github.com/icco
 
-begin
-   require "rubygems"
-rescue LoadError
-   puts "Please install Ruby Gems to continue."
-   exit
-end
-
-# Check all of the gems we need are there.
-[ "sinatra", "less", "sequel" ].each {|gem|
-   begin
-      require gem
-   rescue LoadError
-      puts "The gem #{gem} is not installed.\n"
-      exit
-   end
-}
-
 configure do
    set :sessions, true
    DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://data.db')
