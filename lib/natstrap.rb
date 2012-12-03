0fbb5aa41311b9bd8a2883c80952a43ea7133967
Nat Welch <nat@natwelch.com>
2012-12-02 23:47:45 -0800

    Dicking around.

--- lib/natstrap.rb
+++ lib/natstrap.rb
@@ -1,5 +1,12 @@
 require "natstrap/version"
 
 module Natstrap
-  # Your code goes here...
+  def padrino_init prj_name
+    cmd = "padrino g project #{prj_name} -i -e erb -d activerecord -s jquery -c less"
+    Kernel.system cmd
+  end
+
+  def install_templates prj_name
+
+  end
 end
