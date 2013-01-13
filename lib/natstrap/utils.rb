e6307123adce520cb91904d8cccc5a9ec1178644
Nat Welch <nat@natwelch.com>
2013-01-13 15:37:27 -0800

    switch to Sass

--- lib/natstrap/utils.rb
+++ lib/natstrap/utils.rb
@@ -17,7 +17,7 @@ module Natstrap
     end
 
     def self.create_padrino project_name
-      cmd = "padrino g project #{project_name} -i -e erb -d activerecord -s jquery -c less"
+      cmd = "padrino g project #{project_name} -i -e erb -d activerecord -s jquery -c sass"
       Kernel.system cmd
     end
 
